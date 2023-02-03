9d727d9336e8bcb4b327cc880453b0760fe2dab9
Kyle MacLaughlin <kylemaclaughlin@Kyles-MacBook-Pro-15.local>
2023-02-02 16:45:19 -0800

    Completed Task 4 - Stream Data

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 
+var W3CWebSocket = require('websocket').w3cwebsocket;
+
 class IPDisplay extends React.Component {
   state = {ip: "Loading..."}
   render() {
     return (
-      <div className="IPDisplay">
+      <div className="Data-point">
         IPV{this.props.v6 ? "6" : "4"}: {this.state.ip}<br/>
       </div>
     );
@@ -18,6 +20,39 @@ class IPDisplay extends React.Component {
       .then((text) => {
         this.setState({ip: text})
       });
+  }
+}
+
+class PacketLatency extends React.Component {
+  state = {latency: -1, socket: null}
+  render() {
+    var lat = this.state.latency;
+    return (
+      <div className="Data-point">
+        Packet Latency: {lat >= 0 ? "" + lat + "ms" : "Loading..."}
+      </div>
+    );
+  }
+  componentDidMount() {
+    var socket = new W3CWebSocket("ws://localhost:55455");
+    var comp = this;
+    socket.onerror = (err) => console.log('Connection Error: ' + err.toString());
+    socket.onopen = (connection) => {
+      comp.setState({latency: -1, socket: socket});
+      console.log('WebSocket Connected');
+    };
+    socket.onclose = () => {
+      comp.setState({latency: -1, socket: null});
+      console.log('WebSocket Connection Closed');
+    };
+    socket.onmessage = (message) => {
+      var lat = Date.now() - message.data;
+      comp.setState({latency: lat, socket: socket});
+    };
+  }
+  componentWillUnmount() {
+    console.log("disconnect");
+    if (this.state.socket) this.state.socket.close();
   }
 }
 
@@ -40,9 +75,10 @@ function App() {
       <div className="App-header">
         Sextant
       </div>
-      <Exhibit heading="Heading here...">
+      <Exhibit heading="IP Addresses & Packet Latency">
         <IPDisplay/>
         <IPDisplay v6="true"/>
+        <PacketLatency/>
       </Exhibit>
     </div>
   );
